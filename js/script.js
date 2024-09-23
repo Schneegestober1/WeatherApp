b6d54bc4729b048dc0c5721a31bfd55c45d8b3e2
Kholov Rustam <rustamkholow5@gmail.com>
2024-09-24 00:36:49 +0300

    function checkWeather()

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const apiKey = '21616cbb6c4bc67b09bc62b557f025b8'
+const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q=berlin'
+
+async function checkWeather() {
+    try{
+        const response = await fetch(apiUrl + `&appid=${apiKey}`)
+        if(!response.ok){
+            throw new Error(`Ошибка: ${response.status}`)
+        }
+        const data = await response.json()
+        console.log(data)
+    } catch(error) {
+        console.error(`Не удалось получить данные о погоде: ${error}` )
+    }
+}
+
+checkWeather()
+
